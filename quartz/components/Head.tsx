78a41eb7ac6950e085f06e503db4816a9504c3a5
j z <jz@jdeMacBook-Air.local>
2025-11-20 12:47:46 +0800

    update 2025-11-20-12:47

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -40,6 +40,7 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
+        <meta name="google-site-verification" content="fGQy-3snYWezly1srxGr5NGIEEWSkVoQG7YkI7-eqTg" />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
